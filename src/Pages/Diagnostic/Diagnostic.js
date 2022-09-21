28770d9d6651d467799dd2f325a89e05d025faa6
gauthierpous <gauthier.pous@etud.univ-jfc.fr>
2022-09-21 17:02:01 +0200

    Deuxième version diagnostic

--- src/Pages/Diagnostic/Diagnostic.js
+++ src/Pages/Diagnostic/Diagnostic.js
@@ -61,7 +61,22 @@ function Diagnostic() {
         setTimeout(() => {hiddenGroup.style.display = "none";}, 500);
         shownGroup.style.display = "flex";
         setTimeout(() => {shownGroup.style.opacity = "1";}, 500);
-        isDisplayed++;
+        isDisplayed += 1;
+    }
+
+    function pasDrole(){
+        let g1 = document.getElementById("groupe1");
+        let g2 = document.getElementById("groupe2");
+        let g3 = document.getElementById("groupe3");
+        let g4 = document.getElementById("groupe4");
+        g1.style.opacity = "0";
+        g2.style.opacity = "0";
+        g3.style.opacity = "0";
+        setTimeout(() => {g1.style.display = "none";}, 500);
+        setTimeout(() => {g2.style.display = "none";}, 500);
+        setTimeout(() => {g3.style.display = "none";}, 500);
+        g4.style.display = "flex";
+        setTimeout(() => {g4.style.opacity = "1";}, 500);
     }
 
     return (
@@ -71,28 +86,32 @@ function Diagnostic() {
                 <h2>Compteur de fautes</h2>
             </div>
             <div className="pasDrole">
-                <div><button className="buttonDiagnostic">Je ne suis pas drôle <br /> je veux mes résultats</button></div>
+                <div><button className="buttonDiagnostic" onClick={pasDrole}>Je ne suis pas drôle <br /> je veux mes résultats</button></div>
             </div>
             <img src={logo} id="logo" alt="logo"/>
             <h1>Jouons à un jeu !</h1>
 
             <div id="groupe1" className="group">
-                <h2>Quelle pathologie pensez-vous avoir ?</h2>
-                <div><button className="buttonDiagnostic" onClick={() => setCount(count + 1)}>Mauvaise réponse</button></div>
-                <div><button onClick={change} className="buttonDiagnostic">Bonne réponse</button></div>
-                <div><button className="buttonDiagnostic" onClick={() => setCount(count + 1)}>Mauvaise réponse</button></div>
+                <h2>Comment-vous sentez-vous ?</h2>
+                <div><button className="buttonDiagnostic" onClick={() => setCount(count + 1)}>Bien</button></div>
+                <div><button onClick={change} className="buttonDiagnostic">Malade</button></div>
+                <div><button className="buttonDiagnostic" onClick={() => setCount(count + 1)}>Je sens bon</button></div>
             </div>
             <div id="groupe2" className="group">
-                <h2>Votre espérance de vie a t-elle changé ?</h2>
-                <div><button className="buttonDiagnostic">Cancer des couilles</button></div>
-                <div><button onClick={change} className="buttonDiagnostic">Valider / Inscription</button></div>
-                <div><button className="buttonDiagnostic">Valider / Inscription</button></div>
+                <h2>Votre espérance de vie a t-elle diminué ?</h2>
+                <div><button onClick={change} className="buttonDiagnostic">Oui</button></div>
+                <div><button className="buttonDiagnostic" onClick={() => setCount(count + 1)}>Non</button></div>
+                <div><button className="buttonDiagnostic" onClick={() => setCount(count + 1)}>Non</button></div>
             </div>
             <div id="groupe3" className="group">
-                <h2>Quelle est votre nouvelle espérance de vie ?</h2>
-                <div><button className="buttonDiagnostic">Cancer des couilles</button></div>
-                <div><button className="buttonDiagnostic">Valider / Inscription</button></div>
-                <div><button className="buttonDiagnostic">Valider / Inscription</button></div>
+                <h2>Quelle pathologie pensez-vous avoir ?</h2>
+                <div><button className="buttonDiagnostic" onClick={() => setCount(count + 1)}>Cancer du lobe d'oreille</button></div>
+                <div><button onClick={change} className="buttonDiagnostic">Diabète</button></div>
+                <div><button className="buttonDiagnostic" onClick={() => setCount(count + 1)}>Calvitie de la moustache</button></div>
+            </div>
+            <div id="groupe4" className="group">
+                <h1>Vous êtes effectivement atteint de la donnée envoyée</h1>
+                <h2>Votre score est de {count} fautes ! C'est pas mal du tout</h2>
             </div>
         </div>
     );
